45f2c7cd97cbee228ed2116c940abfb3db5fbdb1
meiamsome <meiamsome@meiamso.me>
2014-09-08 17:37:19 +0100

    Add netlogger example component

---
+++ components/netlogger/netlogger.py
@@ -0,0 +1,8 @@
+from __future__ import print_function
+from tripwire.component import Component
+from tripwire.net.hooks import NetHook
+
+class NetLogger(Component):
+    def enable(self):
+        print("NetLogger enabled.")
+        self._server.register_hook_handler(NetHook, lambda x: print(x.connection_id, x), -10)
